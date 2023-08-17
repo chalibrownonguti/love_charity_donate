757a346b1f4097f5e86914ca9defdbf9b6378a37
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-08-17 20:38:58 +0300

    added changes to src

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ApplicationRecord
-    # has_secure_password
+    has_secure_password
 
-    # devise :database_authenticatable, :registerable,
-    # :recoverable, :rememberable, :validatable,
-    # :omniauthable, omniauth_providers: [:google_oauth2]
-
-
-    # validates :username, presence: true, uniqueness: { case_sensitive: false, message: "username must be unique" }
-    # validates :email, presence: true, uniqueness: { case_sensitive: false, message: "email must be unique" }
-    # validates :password, presence: true, length: { minimum: 6 }
+    devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable,
+    :omniauthable, omniauth_providers: [:google_oauth2]
 
 
-    # def self.from_omniauth(auth)
-    #     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    #       user.email = auth.info.email
-    #       user.password = Devise.friendly_token[0, 20]
-    #       # You can add more fields here if needed
-    #     end
-    # end
+    validates :username, presence: true, uniqueness: { case_sensitive: false, message: "username must be unique" }
+    validates :email, presence: true, uniqueness: { case_sensitive: false, message: "email must be unique" }
+    validates :password, presence: true, length: { minimum: 6 }
+
+
+    def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+          user.email = auth.info.email
+          user.password = Devise.friendly_token[0, 20]
+          # You can add more fields here if needed
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # Devise routes for omniauth callbacks
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  # Regular resources routes for users
   resources :users
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your other application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
